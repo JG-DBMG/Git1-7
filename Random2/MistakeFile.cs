a331017507d35e72729d1320dea1a2d6e6e47e6d
Joe Gastring <Joe.Gastring@DBMGlobal.com>
2023-07-27 11:50:27 -0500

    Same name altered

--- Random2/MistakeFile.cs
+++ Random2/MistakeFile.cs
@@ -6,7 +6,7 @@ namespace RandomNameGenerator
     {
         static void Main(string[] args)
         {
-            string[] firstNames = { "Joe", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabelle", "Jack", "Kate", "Luke", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rose", "Sam", "Tara", "Uma", "Violet", "Will", "Xander", "Yara", "Zane" };
+            string[] firstNames = { "Paige", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabelle", "Jack", "Kate", "Luke", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rose", "Sam", "Tara", "Uma", "Violet", "Will", "Xander", "Yara", "Zane" };
             string[] lastNames = { "Smith", "Johnson", "Brown", "Lee", "Wilson", "Davis", "Miller", "Wilson", "Walker", "Clark", "Lewis", "Taylor", "Hall", "Harris", "Martin", "White", "Green", "Adams", "Allen", "Baker", "Carter", "Evans", "Garcia", "Hayes", "Hill", "King" };
 
             Random random = new Random();
