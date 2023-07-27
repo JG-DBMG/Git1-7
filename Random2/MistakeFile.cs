05504fcaaa2d2fd150666cafa4fbbeb3cea087e8
Joe Gastring <Joe.Gastring@DBMGlobal.com>
2023-07-27 09:32:59 -0500

    Feature branch MistakeFile Addition

--- Random2/MistakeFile.cs
+++ Random2/MistakeFile.cs
@@ -1,1 +1,20 @@
-//Amend me!
+//Amend me! DONE
+using System;
+
+namespace RandomNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNames = { "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabelle", "Jack", "Kate", "Luke", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rose", "Sam", "Tara", "Uma", "Violet", "Will", "Xander", "Yara", "Zane" };
+            string[] lastNames = { "Smith", "Johnson", "Brown", "Lee", "Wilson", "Davis", "Miller", "Wilson", "Walker", "Clark", "Lewis", "Taylor", "Hall", "Harris", "Martin", "White", "Green", "Adams", "Allen", "Baker", "Carter", "Evans", "Garcia", "Hayes", "Hill", "King" };
+
+            Random random = new Random();
+            string firstName = firstNames[random.Next(firstNames.Length)];
+            string lastName = lastNames[random.Next(lastNames.Length)];
+
+            Console.WriteLine($"Random Name: {firstName} {lastName}");
+        }
+    }
+}
